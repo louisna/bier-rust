2692b7149b82401059243b1defc80f01eb7a8dad
Louis Navarre <louisnavarre@hotmail.com>
2022-11-09 15:05:20 +0100

    Bitstring structure and update

--- src/bift.rs
+++ src/bift.rs
@@ -1,7 +1,6 @@
 use serde::{de, Deserialize, Deserializer};
-use serde_json::Value;
 use serde_repr::Deserialize_repr;
-use std::{net::IpAddr, num::ParseIntError, str::FromStr};
+use std::{net::IpAddr, str::FromStr};
 
 #[derive(Deserialize, Debug)]
 pub struct BierState {
@@ -21,22 +20,36 @@ pub struct Bift {
 pub struct BiftEntry {
     /// Bit representing the router of the entry.
     bit: u64,
-    /// All (bitmask, next-hop) pairsfor this bit.
+    /// All (Bitstring, next-hop) pairsfor this bit.
     paths: Vec<BierEntryPath>,
 }
 
 #[derive(Debug, Deserialize)]
 struct BierEntryPath {
-    bitmask: Bitmask,
+    bitstring: Bitstring,
     next_hop: IpAddr,
 }
 
 #[derive(Debug)]
-pub struct Bitmask {
-    bitmask: Vec<u64>,
+pub struct Bitstring {
+    bitstring: Vec<u64>,
 }
 
-impl<'de> Deserialize<'de> for Bitmask {
+impl Bitstring {
+    pub fn update(&mut self, other: &Bitstring, bitop: BitstringOp) {
+        self.bitstring = self
+            .bitstring
+            .iter()
+            .zip(other.bitstring.iter())
+            .map(|(bw_self, bw_other)| match bitop {
+                BitstringOp::And => bw_self & bw_other,
+                BitstringOp::AndNot => bw_self & !bw_other,
+            })
+            .collect();
+    }
+}
+
+impl<'de> Deserialize<'de> for Bitstring {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
@@ -50,26 +63,26 @@ impl<'de> Deserialize<'de> for Bitmask {
     }
 }
 
-impl FromStr for Bitmask {
+impl FromStr for Bitstring {
     type Err = String;
 
-    fn from_str(str_bitmask: &str) -> Result<Self, Self::Err> {
-        let len_of_64_bits = (str_bitmask.len() as f64 / 8.0).ceil() as usize;
+    fn from_str(str_Bitstring: &str) -> Result<Self, Self::Err> {
+        let len_of_64_bits = (str_Bitstring.len() as f64 / 8.0).ceil() as usize;
 
         match (0..len_of_64_bits)
             .map(|i| {
-                let lower_bound = match str_bitmask.len().checked_sub(64 * (i + 1)) {
+                let lower_bound = match str_Bitstring.len().checked_sub(64 * (i + 1)) {
                     Some(v) => v,
                     None => 0,
                 };
-                let upper_bound = usize::min(lower_bound + 64, str_bitmask.len());
-                let substr = &str_bitmask[lower_bound..upper_bound];
+                let upper_bound = usize::min(lower_bound + 64, str_Bitstring.len());
+                let substr = &str_Bitstring[lower_bound..upper_bound];
                 println!("This is the substr: {}", substr);
                 u64::from_str_radix(substr, 2)
             })
             .collect()
         {
-            Ok(v) => Ok(Bitmask { bitmask: v }),
+            Ok(v) => Ok(Bitstring { bitstring: v }),
             Err(e) => Err(format!("Impossible to parse: {:?}", e)),
         }
     }
@@ -82,16 +95,19 @@ pub enum BiftType {
     BierTe = 2,
 }
 
+pub enum BitstringOp {
+    And = 1,
+    AndNot = 2,
+}
+
 #[cfg(test)]
 mod tests {
-    use serde_json::{from_reader, from_value, Value};
 
-    use super::{BierState, BiftType};
-    use std::net::{IpAddr, Ipv6Addr};
+    use super::*;
+    use std::net::IpAddr;
 
-    #[test]
-    fn test_deserialize() {
-        let txt = r#"{"loopback": "fc00::a","bifts": [
+    fn get_dummy_config_json() -> &'static str {
+        r#"{"loopback": "fc00::a","bifts": [
                 {
                     "bift_id": 1,
                     "bift_type": 1,
@@ -101,7 +117,7 @@ mod tests {
                             "bit": 1,
                             "paths": [
                                 {
-                                    "bitmask": "1",
+                                    "bitstring": "1",
                                     "next_hop": "fc00:a::1"
                                 }
                             ]
@@ -110,7 +126,7 @@ mod tests {
                             "bit": 2,
                             "paths": [
                                 {
-                                    "bitmask": "11010",
+                                    "bitstring": "11010",
                                     "next_hop": "fc00:b::1"
                                 }
                             ]
@@ -119,7 +135,7 @@ mod tests {
                             "bit": 3,
                             "paths": [
                                 {
-                                    "bitmask": "11100",
+                                    "bitstring": "11100",
                                     "next_hop": "fc00:c::1"
                                 }
                             ]
@@ -128,11 +144,11 @@ mod tests {
                             "bit": 4,
                             "paths": [
                                 {
-                                    "bitmask": "11010",
+                                    "bitstring": "11010",
                                     "next_hop": "fc00:b::1"
                                 },
                                 {
-                                    "bitmask": "11100",
+                                    "bitstring": "11100",
                                     "next_hop": "fc00:c::1"
                                 }
                             ]
@@ -141,11 +157,11 @@ mod tests {
                             "bit": 5,
                             "paths": [
                                 {
-                                    "bitmask": "11010",
+                                    "bitstring": "11010",
                                     "next_hop": "fc00:b::1"
                                 },
                                 {
-                                    "bitmask": "11100",
+                                    "bitstring": "11100",
                                     "next_hop": "fc00:c::1"
                                 }
                             ]
@@ -154,7 +170,12 @@ mod tests {
                 }
             ]
         }
-        "#;
+        "#
+    }
+
+    #[test]
+    fn test_deserialize() {
+        let txt = get_dummy_config_json();
         let bier_state: BierState = serde_json::from_str(txt).unwrap();
 
         assert_eq!(bier_state.loopback, IpAddr::V6("fc00::a".parse().unwrap()));
@@ -169,8 +190,8 @@ mod tests {
         // Entry 1.
         assert_eq!(bift.entries[0].bit, 1);
         assert_eq!(bift.entries[0].paths.len(), 1);
-        assert_eq!(bift.entries[0].paths[0].bitmask.bitmask.len(), 1);
-        assert_eq!(bift.entries[0].paths[0].bitmask.bitmask[0], 1);
+        assert_eq!(bift.entries[0].paths[0].bitstring.bitstring.len(), 1);
+        assert_eq!(bift.entries[0].paths[0].bitstring.bitstring[0], 1);
         assert_eq!(
             bift.entries[0].paths[0].next_hop,
             IpAddr::V6("fc00:a::1".parse().unwrap())
@@ -179,8 +200,8 @@ mod tests {
         // Entry 2.
         assert_eq!(bift.entries[1].bit, 2);
         assert_eq!(bift.entries[1].paths.len(), 1);
-        assert_eq!(bift.entries[1].paths[0].bitmask.bitmask.len(), 1);
-        assert_eq!(bift.entries[1].paths[0].bitmask.bitmask[0], 26);
+        assert_eq!(bift.entries[1].paths[0].bitstring.bitstring.len(), 1);
+        assert_eq!(bift.entries[1].paths[0].bitstring.bitstring[0], 26);
         assert_eq!(
             bift.entries[1].paths[0].next_hop,
             IpAddr::V6("fc00:b::1".parse().unwrap())
@@ -189,8 +210,8 @@ mod tests {
         // Entry 3.
         assert_eq!(bift.entries[2].bit, 3);
         assert_eq!(bift.entries[2].paths.len(), 1);
-        assert_eq!(bift.entries[2].paths[0].bitmask.bitmask.len(), 1);
-        assert_eq!(bift.entries[2].paths[0].bitmask.bitmask[0], 28);
+        assert_eq!(bift.entries[2].paths[0].bitstring.bitstring.len(), 1);
+        assert_eq!(bift.entries[2].paths[0].bitstring.bitstring[0], 28);
         assert_eq!(
             bift.entries[2].paths[0].next_hop,
             IpAddr::V6("fc00:c::1".parse().unwrap())
@@ -199,14 +220,14 @@ mod tests {
         // Entry 4.
         assert_eq!(bift.entries[3].bit, 4);
         assert_eq!(bift.entries[3].paths.len(), 2);
-        assert_eq!(bift.entries[3].paths[0].bitmask.bitmask.len(), 1);
-        assert_eq!(bift.entries[3].paths[0].bitmask.bitmask[0], 26);
+        assert_eq!(bift.entries[3].paths[0].bitstring.bitstring.len(), 1);
+        assert_eq!(bift.entries[3].paths[0].bitstring.bitstring[0], 26);
         assert_eq!(
             bift.entries[3].paths[0].next_hop,
             IpAddr::V6("fc00:b::1".parse().unwrap())
         );
-        assert_eq!(bift.entries[3].paths[1].bitmask.bitmask.len(), 1);
-        assert_eq!(bift.entries[3].paths[1].bitmask.bitmask[0], 28);
+        assert_eq!(bift.entries[3].paths[1].bitstring.bitstring.len(), 1);
+        assert_eq!(bift.entries[3].paths[1].bitstring.bitstring[0], 28);
         assert_eq!(
             bift.entries[3].paths[1].next_hop,
             IpAddr::V6("fc00:c::1".parse().unwrap())
@@ -215,17 +236,30 @@ mod tests {
         // Entry 5.
         assert_eq!(bift.entries[4].bit, 5);
         assert_eq!(bift.entries[4].paths.len(), 2);
-        assert_eq!(bift.entries[4].paths[0].bitmask.bitmask.len(), 1);
-        assert_eq!(bift.entries[4].paths[0].bitmask.bitmask[0], 26);
+        assert_eq!(bift.entries[4].paths[0].bitstring.bitstring.len(), 1);
+        assert_eq!(bift.entries[4].paths[0].bitstring.bitstring[0], 26);
         assert_eq!(
             bift.entries[4].paths[0].next_hop,
             IpAddr::V6("fc00:b::1".parse().unwrap())
         );
-        assert_eq!(bift.entries[4].paths[1].bitmask.bitmask.len(), 1);
-        assert_eq!(bift.entries[4].paths[1].bitmask.bitmask[0], 28);
+        assert_eq!(bift.entries[4].paths[1].bitstring.bitstring.len(), 1);
+        assert_eq!(bift.entries[4].paths[1].bitstring.bitstring[0], 28);
         assert_eq!(
             bift.entries[4].paths[1].next_hop,
             IpAddr::V6("fc00:c::1".parse().unwrap())
         );
+    }
+
+    #[test]
+    fn test_update_bitstring() {
+        let bitstring = Bitstring::from_str("1101");
+        assert!(bitstring.is_ok());
+        let mut bitstring = bitstring.unwrap();
+        
+        bitstring.update(&Bitstring::from_str("1011").unwrap(), BitstringOp::And);
+        assert_eq!(bitstring.bitstring[0], 0b1001);
+
+        bitstring.update(&Bitstring::from_str("0011").unwrap(), BitstringOp::AndNot);
+        assert_eq!(bitstring.bitstring[0], 0b1000);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
     let file = std::fs::File::open("configs/example.json").expect("Cannot find the file");
     let json: Value = from_reader(file).expect("Cannot read the JSON content");
     debug!("JSON value: {:?}", json);
-    let a: bier_rust::bift::Bitmask = "110".parse().unwrap();
+    let a: bier_rust::bift::Bitstring = "110".parse().unwrap();
     let bier_state: BierState = from_value(json).expect("Cannot parse the JSON to BierState");
     println!("This is the bier state: {:?}", bier_state);
 }
